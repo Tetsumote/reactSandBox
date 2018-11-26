f3a44ab987e318956551f36cf1558fb033e72dda
Paweł Grajewski <unionproject88@gmail.com>
2018-11-26 10:35:54 +0100

    Navigation
    Basic reactstrap navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -13,12 +13,51 @@ import {
   DropdownItem
 } from "reactstrap";
 
-const Navigation = props => {
-  return (
-    <div>
-      <p>This is nav element</p>
-    </div>
-  );
-};
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default Navigation;
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
