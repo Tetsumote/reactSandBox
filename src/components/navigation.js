c95707e1af540f176aa2c0188e057b447e41d364
Paweł Grajewski <unionproject88@gmail.com>
2018-11-26 10:33:22 +0100

    Start
    basic template

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,4 +1,17 @@
 import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 
 const Navigation = props => {
   return (
